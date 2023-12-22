0f1ff273596701b564b887da389318088013f930
Thatguycalledcoder <quarshiedavid62@gmail.com>
2023-12-22 01:48:57 +0000

    Added tests for lowercase, uppercase and punctuation.

--- caesarCipher/caesarCipher.js
+++ caesarCipher/caesarCipher.js
@@ -10,7 +10,25 @@ const caesar = function(input, factor) {
             encryption += " ";
         }
         else {
-            encryption += String.fromCharCode(input.charCodeAt(i) + factor);
+            if (input.charCodeAt(i) >= 65 && input.charCodeAt(i) <= 90) {
+                if (input.charCodeAt(i) + factor > 90) {
+                    encryption += String.fromCharCode(((input.charCodeAt(i) + factor) % 91) + 65)
+                }
+                else {
+                    encryption += String.fromCharCode(input.charCodeAt(i) + factor);
+                }
+            }
+            else if (input.charCodeAt(i) >= 97 && input.charCodeAt(i) <= 122) {
+                if (input.charCodeAt(i) + factor > 122) {
+                    encryption += String.fromCharCode(((input.charCodeAt(i) + factor) % 123) + 97)
+                }
+                else {
+                    encryption += String.fromCharCode(input.charCodeAt(i) + factor);
+                }
+            }
+            else {
+                encryption += input[i];
+            }
         }
     }
     return encryption;
--- caesarCipher/caesarCipher.test.js
+++ caesarCipher/caesarCipher.test.js
@@ -1,5 +1,21 @@
 const caesar = require('./caesarCipher');
 
+it("Mixture of lowercase, uppercase and punctuation", function() {
+    expect(caesar("Hello, World!", 7)).toBe("Olssv, Dvysk!");
+});
+
+it("Mixture of lowercase and uppercase letters", function() {
+    expect(caesar("Hello World", 4)).toBe("Lipps Asvph");
+})
+
+it("Uppercase letters", function() {
+    expect(caesar("HELLO WORLD", 4)).toBe("LIPPS ASVPH");
+})
+
+it("lowercase", function() {
+    expect(caesar("zoo", 3)).toBe("crr");
+})
+
 it("phrase and factor of 3", function() {
     expect(caesar("hello world", 3)).toBe("khoor zruog");
 })
